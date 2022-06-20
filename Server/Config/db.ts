fbd32c5dcd89d5794540849038d29d08d7a548dd
RozhanP <rparando@my.centennialcollege.ca>
2022-06-20 09:48:30 -0400

    initial commit

---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/media";
+export const RemoteURI = "mongodb+srv://Rozhan:ZaleHORPYOXN86rZ@cluster0.2mxzh.mongodb.net/media?retryWrites=true&w=majority";
+export const HostName = "MongoDB Atlas";
+export const Secret = "someSecret";
